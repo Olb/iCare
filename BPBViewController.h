0323db9ddba5d9d1d6aed0ce13e57c95976be816
billy <billy@spartansystems.co>
2014-08-23 18:30:15 -0400

    Still working on PreOp

---
+++ BPBViewController.h
@@ -0,0 +1,13 @@
+//
+//  BPBViewController.h
+//  iCare2
+//
+//  Created by billy bray on 8/22/14.
+//  Copyright (c) 2014 Spartan Systems Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPBViewController : UIViewController
+
+@end
---
+++ BPBViewController.m
@@ -0,0 +1,38 @@
+//
+//  BPBViewController.m
+//  iCare2
+//
+//  Created by billy bray on 8/22/14.
+//  Copyright (c) 2014 Spartan Systems Inc. All rights reserved.
+//
+
+#import "BPBViewController.h"
+
+@interface BPBViewController ()
+
+@end
+
+@implementation BPBViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
